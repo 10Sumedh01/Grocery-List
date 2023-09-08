eb9e2b9c7174c90561639f0cab5abe8eb5450697
Sumedh <sumedhwature2001@gmail.com>
2023-09-09 00:12:59 +0530

    3rd try

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,13 +1,16 @@
 import {FaPlus} from 'react-icons/fa'
+import { useRef } from 'react';  // toggle to the input box after clicking the btn
 import React from "react";
 
 const AddItem = ({newItems, setNewItems, handleSubmit}) => {
+  const inputRef = useRef();
   return (
     <>
       <form className="addForm" onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
         <input
           autoFocus
+          ref={inputRef}  // get cursor from btn to this box
           id="addItem"
           type="text"
           placeholder="Items here"
@@ -18,6 +21,7 @@ const AddItem = ({newItems, setNewItems, handleSubmit}) => {
         <button 
         type="submit"
         aria-label="Add Item"
+        onClick={() => inputRef.current.focus()} // get curson to the input box -->[inputref]
         >
             <FaPlus />
         </button>
